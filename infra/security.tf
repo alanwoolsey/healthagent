742532b93a730fac53772ea003cd78f77151422b
alanwoolsey <46573567+alanwoolsey@users.noreply.github.com>
2025-06-14 08:06:57 -0500

    Update security.tf

--- infra/security.tf
+++ infra/security.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "alb_sg" {
   name        = "alb-sg"
-  description = "Allow inbound HTTP"
+  description = "Allow inbound HTTP traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -16,11 +16,15 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "alb-sg"
+  }
 }
 
 resource "aws_security_group" "ecs_sg" {
   name        = "ecs-sg"
-  description = "Allow ALB -> ECS"
+  description = "Allow traffic from ALB to ECS on port 80"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -35,5 +39,9 @@ resource "aws_security_group" "ecs_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-sg"
   }
 }
